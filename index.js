b2b85db15b38efef3d8c4b70bfab98c131cab7f1
utkarshedim <92948378+utkarshedim@users.noreply.github.com>
2023-09-14 14:40:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.post('/testGads', (req, res) => {
-  console.log(req.body)
+  console.log(req.body,req)
   return res.status(200).json({ status: 'success', message: 'Project Data Available'});
 });
 
